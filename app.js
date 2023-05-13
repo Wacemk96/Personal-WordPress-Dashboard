7f24f1a4edb912eb0bc615af4d0528d4ba0a0c3c
waseem <hello@waseemk.com>
2023-05-13 09:35:03 +0500

    some js and procfile updated

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { url } = require('inspector');
 require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }))
